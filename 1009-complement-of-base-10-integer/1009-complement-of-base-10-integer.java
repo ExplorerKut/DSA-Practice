5acdf08fbd0c831e9262736bcb5752ecd0f242f1
ExplorerKut <53145662+ExplorerKut@users.noreply.github.com>
2022-01-04 13:50:20 +0530

    Time: 0 ms (100.00%), Space: 36.1 MB (27.04%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        // ArrayList<Integer> comp=new ArrayList<Integer>();
+        int count=0;
+        int ans=0;
+        while(n>1){
+            int remainder=n%2;
+            n=n/2;
+            if(remainder!=1){
+                ans+=Math.pow(2,count);
+            }
+            count++;
+        }
+        if(n!=1){
+            ans+=Math.pow(2,count);
+        }
+        return ans;
+    }
+}
