805293a9e4421979fa822255c790369f3b5b021d
ExplorerKut <53145662+ExplorerKut@users.noreply.github.com>
2022-02-03 23:09:46 +0530

    Time: 166 ms (5.41%), Space: 55.1 MB (5.24%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -0,0 +1,44 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int row=1;
+        int col=1;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                   if(matrix[i][j]==0){
+                       matrix[i][0]=0;
+                       matrix[0][j]=0;
+                       if(i==0) row=0;
+                       if(j==0) col=0;
+                   }
+                }
+            }
+        
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+        
+        for(int i=1;i<matrix.length;i++){
+            for(int j=1;j<matrix[0].length;j++){
+                if(matrix[i][0]==0||matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if(row==0){
+            for(int i=0;i<matrix[0].length;i++){
+                matrix[0][i]=0;
+            }
+            
+        }
+        if(col==0){
+            for(int i=0;i<matrix.length;i++){
+                matrix[i][0]=0;
+            }
+        }
+        
+    
+    }
+}
