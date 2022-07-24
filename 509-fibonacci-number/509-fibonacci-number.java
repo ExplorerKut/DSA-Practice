e7405557c8960276d02ecc8461332acaa27785f0
ExplorerKut <53145662+ExplorerKut@users.noreply.github.com>
2022-07-24 15:49:26 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (63.80%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,11 +1,22 @@
 class Solution {
     public int fib(int n) {
+        return fibHelper(n,new int[n+1]);
+    }
+    public int fibHelper(int n,int arr[]){
         if(n==0){
             return 0;
         }
         else if(n==1){
             return 1;
         }
-        return fib(n-1) + fib(n-2);
+        else{
+            if(arr[n]!=0){
+                return arr[n];
+            }
+            else{
+                return arr[n]=fibHelper(n-1,arr)+fibHelper(n-2,arr);
+            }
+        }
     }
+    
 }
